2a62cd85aab0157e63875ba5a1f04cd5dfdb878b
Erfan Abdi <erfangplus@gmail.com>
2020-02-10 16:18:38 +0330

    universal9810: Move symlink creations to Android.mk
    Change-Id: I6501240a6616470c1df7206452ccc4b97ad600d7

--- Android.mk
+++ Android.mk
@@ -7,4 +7,27 @@ ifneq ($(filter crownlte starlte star2lte, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib/libOpenCL.so
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib/libOpenCL.so.1.1
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1
+OPENCL_SYMLINKS += $(TARGET_OUT_VENDOR)/lib64/libOpenCL.so.1.1
+
+$(OPENCL_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(OPENCL_SYMLINKS)
+
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.exynos5.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.exynos5.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf ../egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
+
 endif
